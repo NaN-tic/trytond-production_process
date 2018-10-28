5fb092072202fe46c2f4f7bb334eb003722e03b4
Albert Cervera i Areny <albert@nan-tic.com>
2018-10-28 23:50:56 +0100

    Do not use asterisk in imports.

--- __init__.py
+++ __init__.py
@@ -1,20 +1,20 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .product import *
-from .production import *
+from . import product
+from . import production
 
 
 def register():
     Pool.register(
-        Process,
-        Step,
-        BOMInput,
-        BOMOutput,
-        Operation,
-        Route,
-        BOM,
-        Production,
-        ProductBom,
-        StockMove,
+        production.Process,
+        production.Step,
+        production.BOMInput,
+        production.BOMOutput,
+        production.Operation,
+        production.Route,
+        production.BOM,
+        production.Production,
+        product.ProductBom,
+        production.StockMove,
         module='production_process', type_='model')
